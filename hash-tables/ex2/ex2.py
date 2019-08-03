6eed236e462a820356fb811819a920f68bddc45f
Kendra Williams <kmonique.williams@gmail.com>
2019-08-03 13:38:34 -0400

    completed ex 2

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -15,22 +13,19 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    key = "NONE"
 
     """
     YOUR CODE HERE
     """
+    #add values to hashtable
+    for airport in tickets:
+        hash_table_insert(hashtable, airport.source, airport.destination)
+
+    #create route by following None -> airport codes -> None
+    for val in range(length):
+        airport = hash_table_retrieve(hashtable, key)
+        route[val] = airport
+        key = airport
 
     return route
-
-tickets = [
-  Ticket{ source: "PIT", destination: "ORD" },
-  Ticket{ source: "XNA", destination: "CID" },
-  Ticket{ source: "SFO", destination: "BHM" },
-  Ticket{ source: "FLG", destination: "XNA" },
-  Ticket{ source: "NONE", destination: "LAX" },
-  Ticket{ source: "LAX", destination: "SFO" },
-  Ticket{ source: "CID", destination: "SLC" },
-  Ticket{ source: "ORD", destination: "NONE" },
-  Ticket{ source: "SLC", destination: "PIT" },
-  Ticket{ source: "BHM", destination: "FLG" }
-]
